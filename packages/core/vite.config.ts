05c97cf551f22513a07de36fc72e2fd7cc52659f
Kövesdi György <samy@Kovesdis-MacBook-Air.local>
2025-08-11 13:28:45 +0200

    chore: generate types, fix package info

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'url';
+import dts from 'vite-plugin-dts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+            outDir: 'dist',
+        }),
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
